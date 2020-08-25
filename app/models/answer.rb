5ad12b3858de78cf48b9b4c19c1e2b39abce33b0
fancol <ngocthoson1@gmail.com>
2020-08-25 17:46:37 +0700

    Updating model

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,3 @@
 class Answer < ApplicationRecord
   belongs_to :question
-  has_many :right_answers, class_name: Question, foreign_key: :answer_id, primary_key: :right_answer_id, optional: true
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -2,5 +2,4 @@ class Question < ApplicationRecord
   has_many :exam_questions
   has_many :exams, through: :exam_questions
   has_many :answers
-  belongs_to :answer, class_name: :Answer, foreign_key: :answer_id, primary_key: :right_answer_id, dependent: :destroy
 end
