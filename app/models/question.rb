16eedc889712e89888bf3a80f801511a71e0e5c1
fancol <ngocthoson1@gmail.com>
2020-09-23 01:33:22 +0700

    Finish

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,9 +4,13 @@ class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
 
   accepts_nested_attributes_for :answers, reject_if: :all_blank, allow_destroy: true
-  validate :require_two_answers
+  validate :require_two_answers, :require_one_correct_answer
 
   private
+
+  def require_one_correct_answer
+    errors.add(:base, "You have to add 1 correct answer") unless answers.find_by(correct: true)
+  end
 
   def require_two_answers
     errors.add(:base, "You must provide at least two answers") if answers.size < 2
