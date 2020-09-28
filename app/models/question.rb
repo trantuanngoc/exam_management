d20fa8205df32623ba45975dac0ef78b02ec7b10
fancol <ngocthoson1@gmail.com>
2020-09-28 16:08:30 +0700

    Finish

--- app/models/question.rb
+++ app/models/question.rb
@@ -9,10 +9,10 @@ class Question < ApplicationRecord
   private
 
   def require_one_correct_answer
-    errors.add(:base, "You have to add 1 correct answer") unless answers.any? { |a| a[:correct] == true }
+    errors.add(:base, "You have to add 1 correct answer") unless answers.reject(&:marked_for_destruction?).any? { |a| a[:correct] == true }
   end
 
   def require_two_answers
-    errors.add(:base, "You must provide at least two answers") if answers.size < 2
+    errors.add(:base, "You must provide at least two answers") if answers.reject(&:marked_for_destruction?).size < 2
   end
 end
