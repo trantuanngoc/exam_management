8026d0d2d11bf010a448c60a77c36d2d3b652453
fancol <ngocthoson1@gmail.com>
2020-09-28 16:50:17 +0700

    Finish

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,10 +1,10 @@
 class Profile < ApplicationRecord
   belongs_to :user
   has_one_attached :image
-  validates :image,   content_type: { in: %w[image/jpeg image/gif image/png],
+  validates :image, content_type: { in: %w[image/jpeg image/gif image/png],
     message: "must be a valid image format" },
-size:         { less_than: 5.megabytes,
-    message:   "should be less than 5MB" }
+    size: { less_than: 5.megabytes,
+    message: "should be less than 5MB" }
 
   def full_name
     (last_name || "").capitalize << " " << (first_name || "").capitalize
